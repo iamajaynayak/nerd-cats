f4b30d66e2a9ae67c4198e550d1c62ce17d4ec8e
iamajaynayak <63513168+iamajaynayak@users.noreply.github.com>
2020-06-22 21:47:07 +0530

    Updates

---
+++ precache-manifest.20a1418e31ffd4f2717caf2c00ff4d2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48d4da052a97fc0e16aec0c1263f2335",
+    "url": "/nerd-cats/index.html"
+  },
+  {
+    "revision": "ca02ae095a9b0358a3b1",
+    "url": "/nerd-cats/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4005dd1072885e5ac3c8",
+    "url": "/nerd-cats/static/css/main.497289ba.chunk.css"
+  },
+  {
+    "revision": "ca02ae095a9b0358a3b1",
+    "url": "/nerd-cats/static/js/2.35cf49ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nerd-cats/static/js/2.35cf49ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4005dd1072885e5ac3c8",
+    "url": "/nerd-cats/static/js/main.8cf3bcf8.chunk.js"
+  },
+  {
+    "revision": "3a6c08a77348844280f0",
+    "url": "/nerd-cats/static/js/runtime-main.a76b4f55.js"
+  }
+]);
